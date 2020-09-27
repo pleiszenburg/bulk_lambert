f085c943d50260489c26eb99e6a475bb7a953db0
Sebastian M. Ernst <ernst@pleiszenburg.de>
2020-09-27 22:28:52 +0200

    first draft of bulk lambert

--- bulk_lambert/bulk.py
+++ bulk_lambert/bulk.py
@@ -5,12 +5,15 @@
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 import astropy.units as u
-kms = u.km / u.s
+from astropy.time import TimeDelta
 
 import numpy as np
+from numpy.linalg import norm
 
 from .iod import izzo as izzo_fast
+from ._jit import jit
 from .farnocchia import farnocchia as farnocchia_fast
+from .util import time_range
 
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # PROPAGATE
@@ -18,21 +21,21 @@ from .farnocchia import farnocchia as farnocchia_fast
 
 def propagate(
     orbit,
-    times,
+    epochs,
 ):
-    """Propagate an orbit some times and return the results.
+    """Propagate an orbit some epochs and return the results.
     Parameters
     ----------
     orbit : ~poliastro.twobody.Orbit
         Orbit object to propagate.
-    times_of_flight : ~astropy.time.Time
+    epochs : ~astropy.time.Time
         Time of propagation.
     Returns
     -------
     rr[idx, dim], vv[idx, dim]
     """
 
-    times_of_flight = times - orbit.epoch
+    times_of_flight = epochs - orbit.epoch
 
     # STRIP UNITS
     k = orbit.attractor.k.to(u.km ** 3 / u.s ** 2).value
@@ -41,43 +44,56 @@ def propagate(
     tofs = times_of_flight.to(u.s).value
 
     # Allocate memory
-    rr = np.zeros((times_of_flight.shape[0], 3), dtype = 'f8')
-    vv = np.zeros((times_of_flight.shape[0], 3), dtype = 'f8')
+    shape = (times_of_flight.shape[0], 3)
+    rr = np.zeros(shape, dtype = 'f8')
+    vv = np.zeros(shape, dtype = 'f8')
 
     # Compute
     for idx, tof in enumerate(tofs):
         rr[idx, :], vv[idx, :] = farnocchia_fast(k, r0, v0, tof)
 
     # ADD UNITS
-    return rr * u.km, vv * u.km / u.s
+    return rr * u.km, vv * (u.km / u.s)
 
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # LAMBERT
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 def lambert(
-    orbit_i,
-    orbit_f,
-    epochs,
-    tofs,
-    short=True,
-    M=0,
-    numiter=35,
-    rtol=1e-8,
+    attractor,
+
+    rr_i,
+    vv_i,
+    rr_f,
+    vv_f,
+
+    epoch_start,
+    epoch_stop,
+
+    tof_steps_start: int,
+    tof_steps_stop: int,
+
+    M_max = 2,
+    numiter = 35,
+    rtol = 1e-8,
 ):
     """Computes Lambert maneuver between two different points.
     Parameters
     ----------
+    attractor: Body
 
-    orbit_i: ~poliastro.twobody.Orbit
-        Initial orbit
-    orbit_f: ~poliastro.twobody.Orbit
-        Final orbit
-    method: function
-        Method for solving Lambert's problem
+    rr_i: Initial location
+    vv_i: Initial velocity
+    rr_f: Final location
+    vv_f: Final velocity
 
-    short: keyword, boolean
-        Selects between short and long solution
+    epoch_start: ~astropy.time.Time
+        Time for rr and vv.
+    epoch_stop: ~astropy.time.Time
+        Time for rr and vv.
+
+    tof_steps_start : int
+    tof_steps_stop : int
 
     M : int, optional
         Number of full revolutions, default to 0.
@@ -87,33 +103,87 @@ def lambert(
         Relative tolerance of the algorithm, default to 1e-8.
     """
 
-    # Get initial algorithm conditions
-    k = orbit_i.attractor.k
-    r_i = orbit_i.r
-    r_f = orbit_f.r
+    assert rr_i.shape == rr_f.shape == vv_i.shape == vv_f.shape
+    assert 0 < tof_steps_start
+    assert tof_steps_start <= tof_steps_stop
 
-    # Time of flight is solved by subtracting both orbit epochs
-    tof = orbit_f.epoch - orbit_i.epoch
+    # times
+    epochs = time_range(
+        start = epoch_start,
+        end = epoch_stop,
+    )
+    dt = epochs[1] - epochs[0]
+    epochs_select = epochs[:-tof_steps_stop]
+    epochs_select_len = epochs_select.shape[0]
+    tofs = TimeDelta(
+        [(dt * idx).value for idx in range(tof_steps_start, tof_steps_stop + 1)],
+        scale = dt.scale,
+        format = dt.format,
+    )
 
     # STRIP UNITS
-    k_ = k.to(u.km ** 3 / u.s ** 2).value
-    r_i_ = r_i.to(u.km).value # r0_
-    r_f_ = r.to(u.km).value # r_
-    tof_ = tof.to(u.s).value
+    k_ = attractor.k.to(u.km ** 3 / u.s ** 2).value
+    rr_i_ = rr_i.to(u.km).value
+    vv_i_ = vv_i.to(u.km / u.s).value
+    rr_f_ = rr_f.to(u.km).value
+    vv_f_ = vv_f.to(u.km / u.s).value
+    tofs_ = tofs.to(u.s).value
+
+    # Allocate memory
+    shape = (
+        epochs_select_len,
+        len(range(tof_steps_start, tof_steps_stop + 1)),
+        3,
+    )
+    dv1 = np.zeros(shape, dtype = 'f8')
+    dv2 = np.zeros(shape, dtype = 'f8')
+    dv = np.zeros(shape[:-1], dtype = 'f8')
+    MM = np.zeros(shape[:-1], dtype = 'u8')
 
     # Compute
-    sols = list(izzo_fast(k_, r_i_, r_f_, tof_, M, numiter, rtol))
+    for epoch_idx in range(epochs_select_len):
+
+        for tof_idx, (tof_step, tof_) in enumerate(zip(range(tof_steps_start, tof_steps_stop + 1), tofs_)):
+
+            solutions = []
+
+            for M in range(0, M_max + 1):
+
+                solutions.extend(izzo_fast_(
+                    k_,
+                    rr_i_[epoch_idx, :],
+                    rr_f_[epoch_idx + tof_step, :],
+                    tof_,
+                    M,
+                    numiter,
+                    rtol,
+                ))
+
+            solutions = [
+                (
+                    M,
+                    v1 - vv_i_[epoch_idx, :],
+                    vv_f_[epoch_idx + tof_step, :] - v2,
+                )
+                for M, v1, v2 in solutions
+            ]
+            solutions = [
+                (
+                    M,
+                    v1,
+                    v2,
+                    norm(v1) + norm(v2),
+                )
+                for M, v1, v2 in solutions
+            ]
+            best = min(solutions, key = lambda solution: solutions[3])
+            MM[epoch_idx, tof_idx], dv1[epoch_idx, tof_idx, :], dv2[epoch_idx, tof_idx, :], dv[epoch_idx, tof_idx] = best
 
     # ADD UNITS
-    sols = [((v0 << kms), (v << kms)) for v0, v in sols]
+    return epochs_select, tofs, dv * (u.km / u.s), MM, dv1 * (u.km / u.s), dv2 * (u.km / u.s)
 
-    # Return short or long solution
-    if short:
-        dv_a, dv_b = sols[0]
-    else:
-        dv_a, dv_b = sols[-1]
+@jit
+def izzo_fast_(k, r1, r2, tof, M, numiter, rtol):
 
-    return (
-        (0 * u.s, (dv_a - orbit_i.v).decompose()), # solution #1, decomposed unit
-        (tof.to(u.s), (orbit_f.v - dv_b).decompose()), # solution #2, decomposed unit
-    )
+    for v1, v2 in izzo_fast(k, r1, r2, tof, M, numiter, rtol):
+        yield M, v1, v2
