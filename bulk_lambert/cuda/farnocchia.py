a0525151beaf2d18d2ccab00ead5ed91d85a6de5
Sebastian M. Ernst <ernst@pleiszenburg.de>
2020-09-28 18:52:39 +0200

    cuda does not like numpy

--- bulk_lambert/cuda/farnocchia.py
+++ bulk_lambert/cuda/farnocchia.py
@@ -1,5 +1,6 @@
 
 import numpy as np
+from math import sqrt, pi
 
 from numba import cuda
 
@@ -123,34 +124,34 @@ def nu_from_delta_t(delta_t, ecc, k=1.0, q=1.0, delta=1e-2):
     """
     if ecc < 1 - delta:
         # Strong elliptic
-        n = np.sqrt(k * (1 - ecc) ** 3 / q ** 3)
+        n = sqrt(k * (1 - ecc) ** 3 / q ** 3)
         M = n * delta_t
         # This might represent several revolutions,
         # so we wrap the true anomaly
-        E = M_to_E((M + np.pi) % (2 * np.pi) - np.pi, ecc)
+        E = M_to_E((M + pi) % (2 * pi) - pi, ecc)
         nu = E_to_nu(E, ecc)
     elif 1 - delta <= ecc < 1:
         E_delta = np.arccos((1 - delta) / ecc)
         # We compute M assuming we are in the strong elliptic case
         # and verify later
-        n = np.sqrt(k * (1 - ecc) ** 3 / q ** 3)
+        n = sqrt(k * (1 - ecc) ** 3 / q ** 3)
         M = n * delta_t
         # We check against abs(M) because E_delta could also be negative
         if E_to_M(E_delta, ecc) <= abs(M):
             # Strong elliptic, proceed
             # This might represent several revolutions,
             # so we wrap the true anomaly
-            E = M_to_E((M + np.pi) % (2 * np.pi) - np.pi, ecc)
+            E = M_to_E((M + pi) % (2 * pi) - pi, ecc)
             nu = E_to_nu(E, ecc)
         else:
             # Near parabolic, recompute M
-            n = np.sqrt(k / (2 * q ** 3))
+            n = sqrt(k / (2 * q ** 3))
             M = n * delta_t
             D = M_to_D_near_parabolic(M, ecc)
             nu = D_to_nu(D)
     elif ecc == 1:
         # Parabolic
-        n = np.sqrt(k / (2 * q ** 3))
+        n = sqrt(k / (2 * q ** 3))
         M = n * delta_t
         D = M_to_D(M)
         nu = D_to_nu(D)
@@ -158,7 +159,7 @@ def nu_from_delta_t(delta_t, ecc, k=1.0, q=1.0, delta=1e-2):
         F_delta = np.arccosh((1 + delta) / ecc)
         # We compute M assuming we are in the strong hyperbolic case
         # and verify later
-        n = np.sqrt(k * (ecc - 1) ** 3 / q ** 3)
+        n = sqrt(k * (ecc - 1) ** 3 / q ** 3)
         M = n * delta_t
         # We check against abs(M) because F_delta could also be negative
         if F_to_M(F_delta, ecc) <= abs(M):
@@ -167,14 +168,14 @@ def nu_from_delta_t(delta_t, ecc, k=1.0, q=1.0, delta=1e-2):
             nu = F_to_nu(F, ecc)
         else:
             # Near parabolic, recompute M
-            n = np.sqrt(k / (2 * q ** 3))
+            n = sqrt(k / (2 * q ** 3))
             M = n * delta_t
             D = M_to_D_near_parabolic(M, ecc)
             nu = D_to_nu(D)
     # elif 1 + delta < ecc:
     else:
         # Strong hyperbolic
-        n = np.sqrt(k * (ecc - 1) ** 3 / q ** 3)
+        n = sqrt(k * (ecc - 1) ** 3 / q ** 3)
         M = n * delta_t
         F = M_to_F(M, ecc)
         nu = F_to_nu(F, ecc)
