24a2042580438473d6ede01eb93df179e00261ff
Sebastian M. Ernst <ernst@pleiszenburg.de>
2020-09-28 14:51:37 +0200

    isolated propagator code for cuda

---
+++ bulk_lambert/cuda/elements.py
@@ -0,0 +1,123 @@
+
+import numpy as np
+from numpy import sin, cos, sqrt
+
+from .._jit import jit
+
+from .util import rotation_matrix
+
+@jit
+def rv_pqw(k, p, ecc, nu):
+    r"""Returns r and v vectors in perifocal frame.
+    Parameters
+    ----------
+    k : float
+        Standard gravitational parameter (km^3 / s^2).
+    p : float
+        Semi-latus rectum or parameter (km).
+    ecc : float
+        Eccentricity.
+    nu: float
+        True anomaly (rad).
+    Returns
+    -------
+    r: ndarray
+        Position. Dimension 3 vector
+    v: ndarray
+        Velocity. Dimension 3 vector
+    Notes
+    -----
+    These formulas can be checked at Curtis 3rd. Edition, page 110. Also the
+    example proposed is 2.11 of Curtis 3rd Edition book.
+    .. math::
+        \vec{r} = \frac{h^2}{\mu}\frac{1}{1 + e\cos(\theta)}\begin{bmatrix}
+        \cos(\theta)\\
+        \sin(\theta)\\
+        0
+        \end{bmatrix} \\\\\\
+        \vec{v} = \frac{h^2}{\mu}\begin{bmatrix}
+        -\sin(\theta)\\
+        e+\cos(\theta)\\
+        0
+        \end{bmatrix}
+    Examples
+    --------
+    >>> from poliastro.constants import GM_earth
+    >>> k = GM_earth.value  # Earth gravitational parameter
+    >>> ecc = 0.3  # Eccentricity
+    >>> h = 60000e6  # Angular momentum of the orbit (m**2 / s)
+    >>> nu = np.deg2rad(120)  # True Anomaly (rad)
+    >>> p = h**2 / k  # Parameter of the orbit
+    >>> r, v = rv_pqw(k, p, ecc, nu)
+    >>> # Printing the results
+    r = [-5312706.25105345  9201877.15251336    0] [m]
+    v = [-5753.30180931 -1328.66813933  0] [m]/[s]
+    """
+    pqw = np.array([[cos(nu), sin(nu), 0], [-sin(nu), ecc + cos(nu), 0]]) * np.array(
+        [[p / (1 + ecc * cos(nu))], [sqrt(k / p)]]
+    )
+    return pqw
+
+
+@jit
+def coe_rotation_matrix(inc, raan, argp):
+    """Create a rotation matrix for coe transformation"""
+    r = rotation_matrix(raan, 2)
+    r = r @ rotation_matrix(inc, 0)
+    r = r @ rotation_matrix(argp, 2)
+    return r
+
+
+@jit
+def coe2rv(k, p, ecc, inc, raan, argp, nu):
+    r"""Converts from classical orbital to state vectors.
+    Classical orbital elements are converted into position and velocity
+    vectors by `rv_pqw` algorithm. A rotation matrix is applied to position
+    and velocity vectors to get them expressed in terms of an IJK basis.
+    Parameters
+    ----------
+    k : float
+        Standard gravitational parameter (km^3 / s^2).
+    p : float
+        Semi-latus rectum or parameter (km).
+    ecc : float
+        Eccentricity.
+    inc : float
+        Inclination (rad).
+    omega : float
+        Longitude of ascending node (rad).
+    argp : float
+        Argument of perigee (rad).
+    nu : float
+        True anomaly (rad).
+    Returns
+    -------
+    r_ijk: np.array
+        Position vector in basis ijk.
+    v_ijk: np.array
+        Velocity vector in basis ijk.
+    Notes
+    -----
+    .. math::
+        \begin{align}
+            \vec{r}_{IJK} &= [ROT3(-\Omega)][ROT1(-i)][ROT3(-\omega)]\vec{r}_{PQW}
+                               = \left [ \frac{IJK}{PQW} \right ]\vec{r}_{PQW}\\
+            \vec{v}_{IJK} &= [ROT3(-\Omega)][ROT1(-i)][ROT3(-\omega)]\vec{v}_{PQW}
+                               = \left [ \frac{IJK}{PQW} \right ]\vec{v}_{PQW}\\
+        \end{align}
+    Previous rotations (3-1-3) can be expressed in terms of a single rotation matrix:
+    .. math::
+        \left [ \frac{IJK}{PQW} \right ]
+    .. math::
+        \begin{bmatrix}
+        \cos(\Omega)\cos(\omega) - \sin(\Omega)\sin(\omega)\cos(i) & -\cos(\Omega)\sin(\omega) - \sin(\Omega)\cos(\omega)\cos(i) & \sin(\Omega)\sin(i)\\
+        \sin(\Omega)\cos(\omega) + \cos(\Omega)\sin(\omega)\cos(i) & -\sin(\Omega)\sin(\omega) + \cos(\Omega)\cos(\omega)\cos(i) & -\cos(\Omega)\sin(i)\\
+        \sin(\omega)\sin(i) & \cos(\omega)\sin(i) & \cos(i)
+        \end{bmatrix}
+    """
+    pqw = rv_pqw(k, p, ecc, nu)
+    rm = coe_rotation_matrix(inc, raan, argp)
+
+    ijk = pqw @ rm.T
+
+    return ijk
---
+++ bulk_lambert/cuda/farnocchia.py
@@ -0,0 +1,182 @@
+
+import numpy as np
+
+from .._jit import jit
+
+from .angles import (
+    # D_to_M,
+    D_to_nu,
+    E_to_M,
+    E_to_nu,
+    F_to_M,
+    F_to_nu,
+    M_to_D,
+    M_to_E,
+    M_to_F,
+    # nu_to_D,
+    # nu_to_E,
+    # nu_to_F,
+)
+
+
+@jit
+def D_to_M_near_parabolic(D, ecc):
+    x = (ecc - 1.0) / (ecc + 1.0) * (D ** 2)
+    assert abs(x) < 1
+    S = S_x(ecc, x)
+    return (
+        np.sqrt(2.0 / (1.0 + ecc)) * D + np.sqrt(2.0 / (1.0 + ecc) ** 3) * (D ** 3) * S
+    )
+
+
+@jit
+def _kepler_equation_near_parabolic(D, M, ecc):
+    return D_to_M_near_parabolic(D, ecc) - M
+
+
+@jit
+def _kepler_equation_prime_near_parabolic(D, M, ecc):
+    x = (ecc - 1.0) / (ecc + 1.0) * (D ** 2)
+    assert abs(x) < 1
+    S = dS_x_alt(ecc, x)
+    return np.sqrt(2.0 / (1.0 + ecc)) + np.sqrt(2.0 / (1.0 + ecc) ** 3) * (D ** 2) * S
+
+
+@jit
+def S_x(ecc, x, atol=1e-12):
+    assert abs(x) < 1
+    S = 0
+    k = 0
+    while True:
+        S_old = S
+        S += (ecc - 1 / (2 * k + 3)) * x ** k
+        k += 1
+        if abs(S - S_old) < atol:
+            return S
+
+
+@jit
+def dS_x_alt(ecc, x, atol=1e-12):
+    # Notice that this is not exactly
+    # the partial derivative of S with respect to D,
+    # but the result of arranging the terms
+    # in section 4.2 of Farnocchia et al. 2013
+    assert abs(x) < 1
+    S = 0
+    k = 0
+    while True:
+        S_old = S
+        S += (ecc - 1 / (2 * k + 3)) * (2 * k + 3) * x ** k
+        k += 1
+        if abs(S - S_old) < atol:
+            return S
+
+
+@jit
+def M_to_D_near_parabolic(M, ecc, tol=1.48e-08, maxiter=50):
+    """Parabolic eccentric anomaly from mean anomaly, near parabolic case.
+    Parameters
+    ----------
+    M : float
+        Mean anomaly in radians.
+    ecc : float
+        Eccentricity (~1).
+    Returns
+    -------
+    D : float
+        Parabolic eccentric anomaly.
+    """
+    D0 = M_to_D(M)
+
+    for _ in range(maxiter):
+        fval = _kepler_equation_near_parabolic(D0, M, ecc)
+        fder = _kepler_equation_prime_near_parabolic(D0, M, ecc)
+
+        newton_step = fval / fder
+        D = D0 - newton_step
+        if abs(D - D0) < tol:
+            return D
+
+        D0 = D
+
+    return np.nan
+
+@jit
+def nu_from_delta_t(delta_t, ecc, k=1.0, q=1.0, delta=1e-2):
+    """True anomaly for given elapsed time since periapsis.
+    Parameters
+    ----------
+    delta_t : float
+        Time elapsed since periapsis.
+    ecc : float
+        Eccentricity.
+    k : float
+        Gravitational parameter.
+    q : float
+        Periapsis distance.
+    delta : float
+        Parameter that controls the size of the near parabolic region.
+    Returns
+    -------
+    nu : float
+        True anomaly.
+    """
+    if ecc < 1 - delta:
+        # Strong elliptic
+        n = np.sqrt(k * (1 - ecc) ** 3 / q ** 3)
+        M = n * delta_t
+        # This might represent several revolutions,
+        # so we wrap the true anomaly
+        E = M_to_E((M + np.pi) % (2 * np.pi) - np.pi, ecc)
+        nu = E_to_nu(E, ecc)
+    elif 1 - delta <= ecc < 1:
+        E_delta = np.arccos((1 - delta) / ecc)
+        # We compute M assuming we are in the strong elliptic case
+        # and verify later
+        n = np.sqrt(k * (1 - ecc) ** 3 / q ** 3)
+        M = n * delta_t
+        # We check against abs(M) because E_delta could also be negative
+        if E_to_M(E_delta, ecc) <= abs(M):
+            # Strong elliptic, proceed
+            # This might represent several revolutions,
+            # so we wrap the true anomaly
+            E = M_to_E((M + np.pi) % (2 * np.pi) - np.pi, ecc)
+            nu = E_to_nu(E, ecc)
+        else:
+            # Near parabolic, recompute M
+            n = np.sqrt(k / (2 * q ** 3))
+            M = n * delta_t
+            D = M_to_D_near_parabolic(M, ecc)
+            nu = D_to_nu(D)
+    elif ecc == 1:
+        # Parabolic
+        n = np.sqrt(k / (2 * q ** 3))
+        M = n * delta_t
+        D = M_to_D(M)
+        nu = D_to_nu(D)
+    elif 1 < ecc <= 1 + delta:
+        F_delta = np.arccosh((1 + delta) / ecc)
+        # We compute M assuming we are in the strong hyperbolic case
+        # and verify later
+        n = np.sqrt(k * (ecc - 1) ** 3 / q ** 3)
+        M = n * delta_t
+        # We check against abs(M) because F_delta could also be negative
+        if F_to_M(F_delta, ecc) <= abs(M):
+            # Strong hyperbolic, proceed
+            F = M_to_F(M, ecc)
+            nu = F_to_nu(F, ecc)
+        else:
+            # Near parabolic, recompute M
+            n = np.sqrt(k / (2 * q ** 3))
+            M = n * delta_t
+            D = M_to_D_near_parabolic(M, ecc)
+            nu = D_to_nu(D)
+    # elif 1 + delta < ecc:
+    else:
+        # Strong hyperbolic
+        n = np.sqrt(k * (ecc - 1) ** 3 / q ** 3)
+        M = n * delta_t
+        F = M_to_F(M, ecc)
+        nu = F_to_nu(F, ecc)
+
+    return nu
