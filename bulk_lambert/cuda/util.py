d3ed1d7efe38b6b713b9bf701ea9b4b6cb1da075
Sebastian M. Ernst <ernst@pleiszenburg.de>
2020-09-28 16:04:45 +0200

    jit swap, remove memory allocation

--- bulk_lambert/cuda/util.py
+++ bulk_lambert/cuda/util.py
@@ -1,18 +1,25 @@
 
-from .._jit import jit
+from numba import cuda
 
-import numpy as np
 from numpy import sin, cos
 
-@jit
-def rotation_matrix(angle, axis):
+@cuda.jit(device=True)
+def rotation_matrix(angle, axis, matrix): # matrix must be allocated elsewhere
+
     c = cos(angle)
     s = sin(angle)
+
     if axis == 0:
-        return np.array([[1.0, 0.0, 0.0], [0.0, c, -s], [0.0, s, c]])
+        matrix[0, :] = 1.0, 0.0, 0.0
+        matrix[1, :] = 0.0, c, -s
+        matrix[2, :] = 0.0, s, c
     elif axis == 1:
-        return np.array([[c, 0.0, s], [0.0, 1.0, 0.0], [s, 0.0, c]])
+        matrix[0, :] = c, 0.0, s
+        matrix[1, :] = 0.0, 1.0, 0.0
+        matrix[2, :] = s, 0.0, c
     elif axis == 2:
-        return np.array([[c, -s, 0.0], [s, c, 0.0], [0.0, 0.0, 1.0]])
+        matrix[0, :] = c, -s, 0.0
+        matrix[1, :] = s, c, 0.0
+        matrix[2, :] = 0.0, 0.0, 1.0
     else:
         raise ValueError("Invalid axis: must be one of 'x', 'y' or 'z'")
