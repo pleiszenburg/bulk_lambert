b0bb7ec1786816ab24863ea19448f0e0da402c80
Sebastian M. Ernst <ernst@pleiszenburg.de>
2020-09-28 17:21:44 +0200

    rotation does not allocate memory; naive 3x3 matmul

--- bulk_lambert/cuda/util.py
+++ bulk_lambert/cuda/util.py
@@ -3,23 +3,39 @@ from numba import cuda
 
 from numpy import sin, cos
 
+
 @cuda.jit(device=True)
-def rotation_matrix(angle, axis, matrix): # matrix must be allocated elsewhere
+def matmul_3x3(A, B, _out):
+    """
+    very naive matrix multiplication
+    A, B, _out: 3x3
+    """
+
+    _out[:, :] = 0.0
+
+    for i in range(3):
+        for j in range(3):
+            for k in range(3):
+                _out[i, j] += A[i, k] * B[k, j]
+
+
+@cuda.jit(device=True)
+def rotation_matrix(angle, axis, _matrix): # matrix must be allocated elsewhere
 
     c = cos(angle)
     s = sin(angle)
 
     if axis == 0:
-        matrix[0, :] = 1.0, 0.0, 0.0
-        matrix[1, :] = 0.0, c, -s
-        matrix[2, :] = 0.0, s, c
+        _matrix[0, :] = 1.0, 0.0, 0.0
+        _matrix[1, :] = 0.0, c, -s
+        _matrix[2, :] = 0.0, s, c
     elif axis == 1:
-        matrix[0, :] = c, 0.0, s
-        matrix[1, :] = 0.0, 1.0, 0.0
-        matrix[2, :] = s, 0.0, c
+        _matrix[0, :] = c, 0.0, s
+        _matrix[1, :] = 0.0, 1.0, 0.0
+        _matrix[2, :] = s, 0.0, c
     elif axis == 2:
-        matrix[0, :] = c, -s, 0.0
-        matrix[1, :] = s, c, 0.0
-        matrix[2, :] = 0.0, 0.0, 1.0
+        _matrix[0, :] = c, -s, 0.0
+        _matrix[1, :] = s, c, 0.0
+        _matrix[2, :] = 0.0, 0.0, 1.0
     else:
         raise ValueError("Invalid axis: must be one of 'x', 'y' or 'z'")
